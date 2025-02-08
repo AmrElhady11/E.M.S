37816b5fb9cd87e549952ccc456e86c97b264029
Amr Elhady <amr.elhady.programs@gmail.com>
2025-02-08 17:02:12 +0200

    Update EmployeeDAO.java

--- src/employee/crud/dao/EmployeeDAO.java
+++ src/employee/crud/dao/EmployeeDAO.java
@@ -7,18 +7,13 @@ import employee.crud.beans.Employee;
 
 public interface EmployeeDAO {
 
-	// 1- Insert new Employee
 	public boolean addEmployee(Employee employee) throws SQLException;
 	
-	// 2- Delete an Employee
 	public boolean deleteEmployee(int employeeId) throws SQLException;
 	
-	// 3- Update an Employee
 	public boolean updateEmployee(Employee employee) throws SQLException;
 	
-	// 4- Get an Employee using Employee ID
 	public Employee getEmployee(int employeeId) throws SQLException;
 	
-	// 5- Get All Employees
 	public List<Employee> getAllEmployees() throws SQLException;
 }
